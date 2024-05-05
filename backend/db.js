89f68851bce9d847b1683e502fd119a7be75ab69
killerkc12 <kiranchavankc12@gmail.com>
2024-05-05 14:23:46 +0530

    mongodb added for endpoints

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URL);
+mongoose.connect(
+  "mongodb+srv://mongodb_kiran:XWc8jyBMGeiEQMOR@cluster0.zmaf6to.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+);
+// mongoose.connect(process.env.MONGODB_URL);
 
 const todoSchema = mongoose.Schema({
   title: String,
--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,21 @@
 // write basis boilerplate code with express.json( middleware)
 const express = require('express');
 const { createTodo, updateTodo } = require('./types');
+const todo = require('./db');
 const port = 5000 || process.env.PORT;
 
 const app = express();
 app.use(express.json());
 
 
-app.get('/todos', (req, res) => {
-  console.log('GET /')
-  res.send('Welcome');
+app.get('/todos', async (req, res) => {
+  const todos = await todo.get(); // try find
+  res.json({
+    todos
+  });
 })
 
-app.post('/todo', (req, res) => {
+app.post('/todo', async (req, res) => {
   const createPayload = req.body;
   const parsedPayload = createTodo.safeParse(createPayload);
   if (!parsedPayload.success) {
@@ -22,9 +25,18 @@ app.post('/todo', (req, res) => {
     return;
   }
   // put in mongodb
+  await todo.create({
+    title: createPayload.title,
+    description: createPayload.description,
+    completed: false
+  })
+
+  res.json({
+    message: 'Todo created'
+  });
 })
 
-app.put('/completed', (req, res) => {
+app.put('/completed', async (req, res) => {
   const updatePayload = req.body;
   const parsedPayload = updateTodo.safeParse(updatePayload);
   if (!parsedPayload.success) {
@@ -34,6 +46,15 @@ app.put('/completed', (req, res) => {
     return;
   }
   // update in mongodb
+  await todo.update({
+    _id: req.body.id
+  }, {
+    completed: true
+  })
+
+  res.json({
+    message: 'Todo marked as completed'
+  });
 })
 
 app.listen(port, () => {
